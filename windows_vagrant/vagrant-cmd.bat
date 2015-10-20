ff17bc28d48e850a6ce0c3e3933530dee9bbe99d
Luka Salek <luka.salek@gmail.com>
2015-10-21 00:07:22 +0100

    Added option to pause after vagrant command execution while running vagrant-cmd.bat

--- windows_vagrant/vagrant-cmd.bat
+++ windows_vagrant/vagrant-cmd.bat
@@ -33,24 +33,35 @@ if not defined vagrant_cmd (
 )
 echo ------------------------------------------------------------
 set vagrant_continue=""
-set /p vagrant_continue="Continue? (Y|N): "
+set /p vagrant_continue="Continue? [Y]es | [N]o | Yes and [W]ait when done: "
 echo.
 echo.
 if %vagrant_continue%==Y goto vagrant_continue
 if %vagrant_continue%==y goto vagrant_continue
+if %vagrant_continue%==W goto vagrant_continue
+if %vagrant_continue%==w goto vagrant_continue
 if %vagrant_continue%==N goto vagrant_abort
 if %vagrant_continue%==n goto vagrant_abort
 goto vagrant_start
 
 :vagrant_continue
     vagrant %vagrant_cmd%
+    echo.
+    echo ------------------------------------------------------------
+    if %vagrant_continue%==W goto vagrant_finish_pause
+    if %vagrant_continue%==w goto vagrant_finish_pause
 goto vagrant_finish
 
 :vagrant_abort
+    echo.
+    echo ------------------------------------------------------------
     echo Vagrant command execution aborted
 goto vagrant_finish
 
+:vagrant_finish_pause
+  echo.
+  pause
+  goto vagrant_finish
+  
 :vagrant_finish
-    echo.
-    echo ------------------------------------------------------------
     cd %vagrant_start%
